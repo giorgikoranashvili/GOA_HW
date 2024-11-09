e8d8d61b8714297fb16bfef8ec9aaccc5f1cd1ac
giorgikoranashvili <giorgi.korana@gmail.com>
2024-11-09 18:06:54 +0400

    first commit

---
+++ day.06/homework/hw.py
@@ -0,0 +1,33 @@
+# 1 
+
+# age = int(input("please enter your age: "))
+# print(age)
+
+# future_age = "age" + "future"
+
+# print(f"your future age is: {future_age} age")
+
+# 2
+
+# name =   input("enter your name please: ")
+# weight = input("please enter your kilogram: ")
+# height = input("please enter your height: ")
+# aura = input("please enter your aura: ")
+# age = input("enter your age please: ")
+
+# print()
+
+# 3
+
+# family_members = {}
+
+# num_members = int(input("how many family members are you want?:  "))
+# print(num_members)
+
+# for i in range(num_members):
+#     name = input(f"add {i+1}- family members: ")
+#     age = int(input(f"enter {name}-is age: "))
+#     family_members[name] = age
+
+# for name, age in family_members.items():
+#     print(f"{name} არის {age} წლის.")
