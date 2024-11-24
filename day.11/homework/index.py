e4a6d82554b80ba63cdd25803984050813a3c254
giorgikoranashvili <giorgi.korana@gmail.com>
2024-11-24 14:24:36 +0400

    GOA BEST!

---
+++ day.11/homework/index.py
@@ -0,0 +1,109 @@
+# 1
+# (1)
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for number in numbers:
+    if numbers == numbers:
+        print(True)
+    else:
+        print(False)
+
+# (2)
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for number in numbers:
+    if numbers < numbers:
+        print(True)
+    else:
+        print(False)
+
+# (3)
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for numbers in numbers:
+    if numbers > numbers:
+        print(True)
+    else:
+        print(False)
+
+# 2
+# (1)
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for numbers in numbers:
+    if numbers > 2 or numbers == 0:
+        print(True)
+    else:
+        print(False)
+
+# (2)
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for numbers in numbers:
+    if numbers < 2 or numbers == 0:
+        print(True)
+    else:
+        print(False)
+
+# (3)
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for numbers in numbers:
+    if numbers == 2 or numbers == 0:
+        print(True)
+    else:
+        print(False)
+
+# (4)
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for numbers in numbers:
+    if numbers >= 2 or numbers == 0:
+        print(True)
+    else:
+        print(False)
+
+# (5)
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for numbers in numbers:
+    if numbers <= 2 or numbers == 0:
+        print(True)
+    else:
+        print(False)
+
+# (6)
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for numbers in numbers:
+    if numbers != 2 or numbers == 0:
+        print(True)
+    else:
+        print(False)
+
+numbers = int(input("Please enter your number: "))
+
+for numbers in numbers:
+    if numbers > 10:
+        print("number is high then 10")
+    else: 
+        print("number is not high then ten")
+
+
+# 4 აქ  SOLOLEARN იყო
+
+# 5
+number = int(input("Please enter a number: "))
+
+for number in number:
+    if number < 20 or number > 120:
+        print(True)
+    else:
+        print(False)
+
+
+
+
+
+
+       
