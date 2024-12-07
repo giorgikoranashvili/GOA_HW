354ac830a9ab562e2dae5da23961abd1840a79fc
giorgikoranashvili <giorgi.korana@gmail.com>
2024-12-07 20:37:15 +0400

    GOA BEST!

---
+++ day.07/classwork/indx.py
@@ -0,0 +1,15 @@
+# პირველი საკლასო დავალება
+name = "giorgi"
+age = 11
+height = 1.53
+print(name, age, height)
+
+# მეორე საკლასო დავალება
+name = "giorgi"
+age = 11
+height = 1.53
+input("what is your name?")
+input("what is your age?")
+input("what is your age?")
+
+
---
+++ day.11/classwork/index.py
@@ -0,0 +1,7 @@
+number = int(input("Please enter your number: "))
+
+for i in range(1, 0):
+    if number <= 2:
+        print(True)
+    else:
+        print(False)
