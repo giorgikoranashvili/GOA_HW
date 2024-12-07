51079c077636cd104a9afba60e8abe17c5767fee
giorgikoranashvili <giorgi.korana@gmail.com>
2024-12-07 20:48:36 +0400

    Goa best!

---
+++ day.13/homework/index.py
@@ -0,0 +1,34 @@
+# 1) უყურეთ ჩანაწერს თავიდან! 
+
+# 2) დაწერეთ პროგრამა სადაც შედარებების და ლოგიკური ოპერატორების მეშვეობით 10 ჯერ გამოიტანეთ True
+print("True")
+print("True")
+print("True")
+print("True")
+print("True")
+print("True")
+print("True")
+print("True")
+print("True")
+print("True")
+
+# 3)დაწერეთ პროგრამა სადაც შედარებების და ლოგიკური ოპერატორების მეშვეობით 10 ჯერ გამოიტანეთ False
+print("False")
+print("False")
+print("False")
+print("False")
+print("False")
+print("False")
+print("False")
+print("False")
+print("False")
+print("False")
+
+# 4) კომენტარის სახით ახსენი რა არის sequence
+# sequence is like sequence
+
+# 5)კომენტარის სახით ახსენი რა არის selection
+# selection aris rogorc acheva
+
+# 6)კომენტარის სახით ახსენი რა არის iteration 
+# iteration  aris rogorc iteratori
