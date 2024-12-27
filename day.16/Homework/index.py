16f23039e399663982341f8c22f056eb21728448
giorgikoranashvili <giorgi.korana@gmail.com>
2024-12-27 17:07:52 +0400

    GOA BEST!

---
+++ day.16/Homework/index.py
@@ -0,0 +1,14 @@
+# while loop ის გამოყენებით დაბეჭდეთ ყველა რიცხვი 1 დან 10 მდე
+for i in range(1, 11):
+    print(i)
+
+# while loop ის გამოყენებით დაწერეთ პაროლის შემოწმების კოდი, მომხმარებეს შემოატანინეთ პაროლი და შემდეგ იქამდე ჰკითხეთ შეიყვანოს მეორედ სანამ იგივეს არ შეიყვას
+password = "giorgi1234"
+
+for password in password:
+    print("Your password is corect!")
+else:
+    print("Your password is wrong")
+
+
+# აუცილებლად უყურეთ ჩანაწერს
