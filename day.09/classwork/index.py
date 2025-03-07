6a838c66273bca4ba313948504df5eee16fcdbef
giorgikoranashvili <giorgi.korana@gmail.com>
2025-03-07 15:39:10 +0400

    GOA BEST!

---
+++ day.09/classwork/index.py
@@ -0,0 +1,25 @@
+# 1
+# სამი ცვლადის შექმნა
+int_var = 10        # integer ტიპის ცვლადი
+float_var = 20.5    # float ტიპის ცვლადი
+string_var = "30"   # string ტიპის ცვლადი
+
+# ფორმატის შეცვლა და გამოყვანა
+int_to_string = str(int_var)         # integer -> string
+string_to_int = int(string_var)      # string -> integer
+int_to_float = float(int_var)        # integer -> float
+
+print(f"Integer to String: {int_to_string}")
+print(f"String to Integer: {string_to_int}")
+print(f"Integer to Float: {int_to_float}")
+
+# 2
+# სამი ცვლადის შექმნა
+int_var = 10        # integer ტიპის ცვლადი
+float_var = 20.5    # float ტიპის ცვლადი
+string_var = "30"   # string ტიპის ცვლადი
+
+# მათი ტიპის გამოყვანა
+print(f"Type of int_var: {type(int_var)}")
+print(f"Type of float_var: {type(float_var)}")
+print(f"Type of string_var: {type(string_var)}")
