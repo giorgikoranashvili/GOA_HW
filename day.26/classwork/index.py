77bfcac059a5040796194f9db32c2e362f8d0c9c
giorgikoranashvili <giorgi.korana@gmail.com>
2025-03-07 15:12:24 +0400

    GOA BEST!

---
+++ day.26/classwork/index.py
@@ -0,0 +1,13 @@
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+my_list[0] = 'A'
+my_list[2] = 'B'
+my_list[4] = 'C'
+my_list[6] = 'D'
+my_list[8] = 'E'
+
+first_element = my_list[0]
+print(f"პირველი ელემენტი: {first_element}")
+
+last_element = my_list[-1]
+print(f"ბოლო ელემენტი: {last_element}")
