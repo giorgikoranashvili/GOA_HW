8a37be9a0ef7634176de327853ed4e7849d7280f
giorgikoranashvili <giorgi.korana@gmail.com>
2025-04-09 19:18:26 +0400

    second commit

---
+++ day.31/classwork/index.py
@@ -0,0 +1,18 @@
+# მეორე საკლასო დავალება
+def func1(world, times):
+    print(world * times)
+
+
+# მესამე საკლასო დავალება
+def calculator(num1,num2,operator):
+    if operator == '+':
+        print(num1 + num2)
+    elif operator == '-':
+        print(num1 - num2)
+    elif operator == '*':
+        print(num1 * num2)
+    elif operator == '/':
+        print(num1 / num2)
+calculator(5,5,"+")       
+
+
---
+++ day.31/homework/index.py
@@ -0,0 +1,17 @@
+# 1
+# ფუნქცია არის კოდის ბლოკი, რომელიც გვაძლევს საშუალებას:
+
+# 2
+def nums(n1, n2):
+    result = (n1 + n2) * 2
+    return result
+
+# 3
+def avragecount(n1, n2):
+    result = (n1 + n2) / 2
+    return result
+
+# 4
+def func1(n1, n2, n3):
+    result = (n1 * n2) / n3
+    return result
