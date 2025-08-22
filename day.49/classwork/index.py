aa8840ca655c8aaf38d7759fa4bf2e13b3c668d2
giorgikoranashvili <giorgi.korana@gmail.com>
2025-08-22 14:22:25 +0400

    GOA BEST

---
+++ day.49/classwork/index.py
@@ -0,0 +1,17 @@
+def my_func(text, times):
+    print(text * times)
+
+
+
+def my_func(flag, text):
+    if flag:
+        print(text)    
+
+
+def my_func(my_list, index):
+    print(my_list[0:index])
+
+
+
+def my_func(number):
+    print(number ** 3)            
