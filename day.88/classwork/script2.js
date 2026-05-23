a1646fd57b0b4a1f2cfde9538bdeee713c83e018
giorgikoranashvili <giorgi.korana@gmail.com>
2026-05-23 14:52:48 +0400

    GOA BEST

---
+++ day.88/classwork/script2.js
@@ -0,0 +1,9 @@
+const myMoney = 50;
+
+const itemPrice = 30;
+
+if (myMoney >= itemPrice) {
+    console.log("თქვინ შეგიძლიათ ამ ნივთის ყიდვა");
+} else {
+    console.log("სამწუხაროდ თანხა არ გყოფნით")
+}
